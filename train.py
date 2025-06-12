6b7999bd638a801ffbe992ca00d3b3f266de9b54
fabianescobar12 <fabian.escobar@garcesfruit.com>
2025-06-12 18:20:12 -0400

    Update train.py

--- train.py
+++ train.py
@@ -3,10 +3,10 @@ from ultralytics import YOLO
 model = YOLO("yolo11s.pt")          # carga peso base COCO
 model.train(
     data="configs/cherries_maturity.yaml",
-    epochs=5,
-    imgsz=640,
+    epochs=120,
+    imgsz=800,
     batch=16,
-    fraction=0.2,
+    fraction=1.0,
     project="cherry_yolo11",
     name="maturity_combined",
 )
